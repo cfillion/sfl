e9baa43624608ce95e4a29e537fc9f05e0bb7bcd
cfillion <cfillion@users.noreply.github.com>
2015-07-20 16:53:09 -0400

    remove impossible condition
    the condition would only be true if the number of allowed types in
    value_variant would mismatch the size of the enum

--- src/variable.cpp
+++ src/variable.cpp
@@ -11,16 +11,9 @@ variable::variable(const std::string &name, const enum type t,
 variable::variable(const std::string &name, const value_variant &v,
     const class location &loc)
   : m_name(name), m_value(v), m_type(m_value.type()),
+    m_type_id(static_cast<enum type>(m_value.which())),
     m_location(loc), m_undefined(false)
-{
-  const int which = m_value.which();
-
-  // this would only happen if we forgot to update the type enum
-  if(which > std::numeric_limits<enum type>::max())
-    throw invalid_assignment();
-
-  m_type_id = (enum type)which;
-}
+{}
 
 std::type_index variable::type_of(const enum type t) const
 {
