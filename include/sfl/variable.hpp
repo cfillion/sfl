e82791888f264f53d151e136b32552ab2b1bb022
cfillion <cfillion@users.noreply.github.com>
2015-07-20 17:10:03 -0400

    don't compare types directly anymore

--- include/sfl/variable.hpp
+++ include/sfl/variable.hpp
@@ -7,7 +7,6 @@
 
 #include <boost/variant.hpp>
 #include <string>
-#include <typeindex>
 
 namespace sfl {
   typedef boost::variant<std::string, int> value_variant;
@@ -24,7 +23,7 @@ namespace sfl {
       const class location &loc = sfl::location());
 
     const std::string &name() const { return m_name; }
-    enum type type() const { return m_type_id; }
+    enum type type() const { return m_type; }
     const class location &location() const { return m_location; }
     bool empty() const { return m_undefined; }
 
@@ -34,7 +33,7 @@ namespace sfl {
       // creating a temporary variant allows the assignment of compatible
       // but not strictly equal types (eg. a const char * in a string variable)
       const value_variant test{val};
-      if(std::type_index(test.type()) != m_type)
+      if(test.which() != m_type)
         throw invalid_assignment();
 
       m_value = val;
@@ -45,21 +44,21 @@ namespace sfl {
     template<typename T>
     const T &get() const
     {
-      if(std::type_index(typeid(T)) != m_type)
-        throw invalid_conversion();
-      else if(m_undefined)
+      if(m_undefined)
         throw undefined_value();
 
-      return boost::get<T>(m_value);
+      try {
+        return boost::get<T>(m_value);
+      }
+      catch(const boost::bad_get &) {
+        throw invalid_conversion();
+      }
     }
 
   private:
-    std::type_index type_of(const enum type) const;
-
     std::string m_name;
     value_variant m_value;
-    std::type_index m_type;
-    enum type m_type_id;
+    enum type m_type;
     class location m_location;
     bool m_undefined;
   };
--- src/variable.cpp
+++ src/variable.cpp
@@ -4,21 +4,11 @@ using namespace sfl;
 
 variable::variable(const std::string &name, const enum type t,
     const class location &loc)
-  : m_name(name), m_type(type_of(t)), m_type_id(t),
-    m_location(loc), m_undefined(true)
+  : m_name(name), m_type(t), m_location(loc), m_undefined(true)
 {}
 
 variable::variable(const std::string &name, const value_variant &v,
     const class location &loc)
-  : m_name(name), m_value(v), m_type(m_value.type()),
-    m_type_id(static_cast<enum type>(m_value.which())),
+  : m_name(name), m_value(v), m_type(static_cast<enum type>(m_value.which())),
     m_location(loc), m_undefined(false)
 {}
-
-std::type_index variable::type_of(const enum type t) const
-{
-  switch(t) {
-  case string:
-    return typeid(std::string);
-  }
-}
